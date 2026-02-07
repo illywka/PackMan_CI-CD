052ccdda389e3530b4f61dd4db3dfa5a810ec7a4
Artemko <productionkrey@gmail.com>
2026-02-08 00:03:15 +0200

    Implement basic Pacman class and game constants

---
+++ src/entities/pacman.py
@@ -0,0 +1,14 @@
+import pygame
+from src.utils.constants import TILE_SIZE, YELLOW
+
+class PacMac(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+
+        self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def update(self):
+        pass
