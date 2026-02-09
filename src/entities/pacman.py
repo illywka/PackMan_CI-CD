36029f0f548162cded1285467e3e0b3813f2ec7a
Artemko <productionkrey@gmail.com>
2026-02-09 20:21:18 +0200

    Implement wall collision and grid snapping

--- src/entities/pacman.py
+++ src/entities/pacman.py
@@ -29,12 +29,52 @@ class Pacman(pygame.sprite.Sprite):
         elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
             self.next_direction = pygame.Vector2(0, 1)
 
+    def check_collision(self, direction):
+        next_rect = self.rect.copy()
+        next_rect.move_ip(direction * self.speed)
+
+        if next_rect.collidelist(self.walls) > -1:
+            return True
+        
+        return False
+    
+    def is_centered(self):
+        center_x, center_y = self.rect.center
+
+        tile_center_x = (center_x // TILE_SIZE) * TILE_SIZE + TILE_SIZE // 2    
+        tile_center_y = (center_y // TILE_SIZE) * TILE_SIZE + TILE_SIZE // 2
+
+        dist_x = abs(center_x - tile_center_x)
+        dist_y = abs(center_y - tile_center_y)
+
+        tolerance = self.speed
+
+        return dist_x <= tolerance and dist_y <= tolerance
+
     def move(self):
         if self.next_direction != pygame.Vector2(0, 0):
-            self.direction = self.next_direction
+            if self.next_direction == -self.direction:
+                self.direction = self.next_direction
+                self.next_direction = pygame.Vector2(0, 0)
 
-        self.pos += self.direction * self.speed
-        self.rect.topleft = self.pos.x, self.pos.y
+            elif self.is_centered():
+                if self.direction != self.next_direction:
+                    if not self.check_collision(self.next_direction):
+                        current_tile_x = (self.rect.centerx // TILE_SIZE) * TILE_SIZE
+                        current_tile_y = (self.rect.centery // TILE_SIZE) * TILE_SIZE
+
+                        self.pos.x = current_tile_x
+                        self.pos.y = current_tile_y
+                        self.rect.topleft = (self.pos.x, self.pos.y)
+
+                        self.direction = self.next_direction
+                        self.next_direction = pygame.Vector2(0, 0)
+
+        if not self.check_collision(self.direction):
+            self.pos += self.direction * self.speed
+            self.rect.topleft = self.pos.x, self.pos.y
+        else:
+            self.rect.topleft = self.pos.x, self.pos.y
 
     def update(self):
         self.get_input()
