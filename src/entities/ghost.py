54d3dde7cd3fa6f13eaf54fca2bb29bb820b38bd
ilywka_ <illasalnikov6@gmail.com>
2026-02-12 11:09:08 +0200

    ghosts-now-can-teleport

--- src/entities/ghost.py
+++ src/entities/ghost.py
@@ -79,18 +79,54 @@ class Ghost(pygame.sprite.Sprite, ABC):
             current = queue.popleft()
 
             if current == goal:
-                return self.reconstruct_path(visited, goal)
+                return self.reconstruct_path(visited, goal, rows, cols)
                 
             for dr, dc in directions:
-                neighbor = (current[0] + dr, current[1] + dc)
-                r, c = neighbor
-
-                if 0 <= r < rows and 0 <= c < cols:
-                    if matrix[r][c] == 0 and neighbor not in visited:
-                        visited[neighbor] = current
-                        queue.append(neighbor)
+                r, c = current[0] + dr, current[1] + dc
+                
+                if r < 0:
+                    r = rows - 1
+                elif r >= rows:
+                    r = 0
+                
+                if c < 0:
+                    c = cols - 1
+                elif c >= cols:
+                    c = 0
+                
+                neighbor = (r, c)
+                
+                if matrix[r][c] == 0 and neighbor not in visited:
+                    visited[neighbor] = current
+                    queue.append(neighbor)
         
         return None  
+
+    def reconstruct_path(self, visited, goal, rows, cols):
+        path = []
+        curr = goal
+        while curr is not None:
+            path.append(curr)
+            curr = visited[curr]
+        
+        dir_list = []
+
+        for i in range(len(path)-1):
+            target = path[i]
+            source = path[i+1]
+            
+            dy = target[0] - source[0]
+            dx = target[1] - source[1]
+
+            if dy > 1:  dy = -1 
+            elif dy < -1: dy = 1 
+            
+            if dx > 1:  dx = -1 
+            elif dx < -1: dx = 1 
+
+            dir_list.append((dx, dy))
+            
+        return dir_list[::-1]
 
     def bfs_dodge_pacman(self, matrix, start, goal):
         rows, cols = len(matrix), len(matrix[0])
@@ -103,30 +139,28 @@ class Ghost(pygame.sprite.Sprite, ABC):
             current = queue.popleft()
 
             if current == goal:
-                return self.reconstruct_path(visited, goal)
+                return self.reconstruct_path(visited, goal, rows, cols)
                 
             for dr, dc in directions:
-                neighbor = (current[0] + dr, current[1] + dc)
-                r, c = neighbor
-
-                if 0 <= r < rows and 0 <= c < cols:
-                    if matrix[r][c] == 0 and pygame.Vector2(c*TILE_SIZE, r*TILE_SIZE) != self.pacman.pos and neighbor not in visited:
-                        visited[neighbor] = current
-                        queue.append(neighbor)
+                r, c = current[0] + dr, current[1] + dc
+                
+                if r < 0:
+                    r = rows - 1
+                elif r >= rows:
+                    r = 0
+                
+                if c < 0:
+                    c = cols - 1
+                elif c >= cols:
+                    c = 0
+                
+                neighbor = (r, c)
+                
+                if matrix[r][c] == 0 and pygame.Vector2(c*TILE_SIZE, r*TILE_SIZE) != self.pacman.pos and neighbor not in visited:
+                    visited[neighbor] = current
+                    queue.append(neighbor)
         
         return None  
-
-
-    def reconstruct_path(self, visited, goal):
-        path = []
-        curr = goal
-        while curr is not None:
-            path.append(curr)
-            curr = visited[curr]
-        dir = []
-        for i in range(len(path)-1):
-            dir.append((path[i][1]-path[i+1][1], path[i][0]-path[i+1][0]))
-        return dir[::-1]
 
     def future_pos(self, pos, dir):
         return int(pos[0])//TILE_SIZE + int(dir[1]), int(pos[1])//TILE_SIZE + int(dir[0])
